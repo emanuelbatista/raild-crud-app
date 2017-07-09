c450769dc561602c487c7a7b97a4512fb5174284
emanuelbatista <emanuelbatista2011@gmail.com>
2017-07-09 15:48:58 -0300

    adding input slider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709181359) do
+ActiveRecord::Schema.define(version: 20170709184309) do
 
   create_table "carros", force: :cascade do |t|
     t.string   "modelo"
